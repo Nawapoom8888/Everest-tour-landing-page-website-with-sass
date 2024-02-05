7cb139d8c2a4905c8bc1bd81eb26b75814ebf330
Nawapoom Panichwilai <nawapoom.panichwilai@gmail.com>
2024-02-05 17:41:21 +0700

    fixed image src

--- src/components/ImageGrid.jsx
+++ src/components/ImageGrid.jsx
@@ -4,22 +4,22 @@ export default function ImageGrid() {
   return (
     <div className="image-grid">
       <img
-        src="../../public/images/img-grid-01.avif"
+        src="../../images/img-grid-01.avif"
         alt="image-01"
         className="img-1"
       />
       <img
-        src="../../public/images/img-grid-02.avif"
+        src="../../images/img-grid-02.avif"
         alt="image-02"
         className="img-2"
       />
       <img
-        src="../../public/images/img-grid-03.avif"
+        src="../../images/img-grid-03.avif"
         alt="image-03"
         className="img-3"
       />
       <img
-        src="../../public/images/img-grid-04.avif"
+        src="../../images/img-grid-04.avif"
         alt="image-04"
         className="img-4"
       />
